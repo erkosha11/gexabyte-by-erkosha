9bc81c7240a63fb55210b4f939fe4fd10a82e397
Erkosha <erkebulan003@yandex.ru>
2024-04-23 08:38:54 +0500

    add products block

--- src/App/router.tsx
+++ src/App/router.tsx
@@ -2,6 +2,7 @@ import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
 import { About } from "../pages/About/About";
 import { Home } from "../pages/Home/Home";
+import { Products } from "../pages/Products/Products";
 import "./router.scss";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Home />
       <About/>
+      <Products/>
       <Footer />
     </>
   );
