1bc99d24f7cc2a4ed98455fe5819ac9cd899a712
Erkosha <erkebulan003@yandex.ru>
2024-04-24 02:25:49 +0500

    changed about

---
+++ src/pages/About/components/AboutCards.tsx
@@ -0,0 +1,62 @@
+import s from "../About.module.scss";
+
+const AboutCards = () => {
+  return (
+    <div className={s.aboutCards}>
+      <div className={s.aboutCardsWrapper}>
+        <div className={s.item}>
+          <p>Криптовалютные биржи</p>
+        </div>
+        <div className={s.item}>
+          <p>Маркетплейсы</p>
+        </div>
+        <div className={s.item}>
+          <p>DeFi/CeFi кошельки</p>
+        </div>
+        <div className={s.item}>
+          <p>Образовательные платформы</p>
+        </div>
+        <div className={s.item}>
+          <p>Launchpad платформы</p>
+        </div>
+        <div className={s.item}>
+          <p>Финтех продукты</p>
+        </div>
+        <div className={s.item}>
+          <p>CRM системы</p>
+        </div>
+        <div className={s.item}>
+          <p>NFT-маркетплейсы</p>
+        </div>
+      </div>
+      <div className={s.aboutCardsWrapper}>
+        <div className={s.item}>
+          <p>Криптовалютные биржи</p>
+        </div>
+        <div className={s.item}>
+          <p>Маркетплейсы</p>
+        </div>
+        <div className={s.item}>
+          <p>DeFi/CeFi кошельки</p>
+        </div>
+        <div className={s.item}>
+          <p>Образовательные платформы</p>
+        </div>
+        <div className={s.item}>
+          <p>Launchpad платформы</p>
+        </div>
+        <div className={s.item}>
+          <p>Финтех продукты</p>
+        </div>
+        <div className={s.item}>
+          <p>CRM системы</p>
+        </div>
+        <div className={s.item}>
+          <p>NFT-маркетплейсы</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutCards;
